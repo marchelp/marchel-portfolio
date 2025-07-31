4e362d763c2f2a8fa91eee0ca39a7ecd72b4db0f
marchelp <marchel.adias@gmail.com>
2025-07-31 09:13:39 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,308 @@
 import Image from "next/image";
+import Lanyard from "./components/Lanyard/Lanyard";
+import RotatingText from "./components/RotatingText/RotatingText";
+import SplitText from "./components/SplitText/SplitText";
+import BlurText from "./components/BlurText/BlurText";
+import AnimatedContent from "./components/AnimatedContent/AnimatedContent";
+import Squares from "./components/Squares/Squares";
+// import SplashCursor from "./components/SplashCursor/SplashCursor";
+import GradientText from "./components/GradientText/GradientText";
+import CircularText from "./components/CircularText/CircularText";
+import ScrollVelocity from "./components/ScrollVelocity/ScrollVelocity";
+import { Timeline } from "./components/Timeline/Timeline";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const data = [
+    {
+      title: "2025",
+      content: (
+        <div>
+          <h2 className="font-bold text-xl my-2">
+            April - June 
+          </h2>
+          <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
+            MAGENTA Batch 2 
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <img
+              src="images/experience/sptp-1.jpg"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <img
+              src="images/experience/sptp-2.jpg"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+            />
+            <img
+              src="images/experience/sptp-3.jpg"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+            />
+            <img
+              src="images/experience/sptp-4.jpg"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+            />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ),
+    },
+    // {
+    //   title: "Early 2023",
+    //   content: (
+    //     <div>
+    //       <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
+    //         I usually run out of copy, but when I see content this big, I try to
+    //         integrate lorem ipsum.
+    //       </p>
+    //       <p className="mb-8 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
+    //         Lorem ipsum is for people who are too lazy to write copy. But we are
+    //         not. Here are some more example of beautiful designs I built.
+    //       </p>
+    //       <div className="grid grid-cols-2 gap-4">
+    //         <img
+    //           src="https://assets.aceternity.com/pro/hero-sections.png"
+    //           alt="hero template"
+    //           width={500}
+    //           height={500}
+    //           className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+    //         />
+    //         <img
+    //           src="https://assets.aceternity.com/features-section.png"
+    //           alt="feature template"
+    //           width={500}
+    //           height={500}
+    //           className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+    //         />
+    //         <img
+    //           src="https://assets.aceternity.com/pro/bento-grids.png"
+    //           alt="bento template"
+    //           width={500}
+    //           height={500}
+    //           className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+    //         />
+    //         <img
+    //           src="https://assets.aceternity.com/cards.png"
+    //           alt="cards template"
+    //           width={500}
+    //           height={500}
+    //           className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+    //         />
+    //       </div>
+    //     </div>
+    //   ),
+    // },
+    // {
+    //   title: "Changelog",
+    //   content: (
+    //     <div>
+    //       <p className="mb-4 text-xs font-normal text-neutral-800 md:text-sm dark:text-neutral-200">
+    //         Deployed 5 new components on Aceternity today
+    //       </p>
+    //       <div className="mb-8">
+    //         <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
+    //           ✅ Card grid component
+    //         </div>
+    //         <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
+    //           ✅ Startup template Aceternity
+    //         </div>
+    //         <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
+    //           ✅ Random file upload lol
+    //         </div>
+    //         <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
+    //           ✅ Himesh Reshammiya Music CD
+    //         </div>
+    //         <div className="flex items-center gap-2 text-xs text-neutral-700 md:text-sm dark:text-neutral-300">
+    //           ✅ Salman Bhai Fan Club registrations open
+    //         </div>
+    //       </div>
+    //       <div className="grid grid-cols-2 gap-4">
+    //         <img
+    //           src="https://assets.aceternity.com/pro/hero-sections.png"
+    //           alt="hero template"
+    //           width={500}
+    //           height={500}
+    //           className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+    //         />
+    //         <img
+    //           src="https://assets.aceternity.com/features-section.png"
+    //           alt="feature template"
+    //           width={500}
+    //           height={500}
+    //           className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+    //         />
+    //         <img
+    //           src="https://assets.aceternity.com/pro/bento-grids.png"
+    //           alt="bento template"
+    //           width={500}
+    //           height={500}
+    //           className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+    //         />
+    //         <img
+    //           src="https://assets.aceternity.com/cards.png"
+    //           alt="cards template"
+    //           width={500}
+    //           height={500}
+    //           className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
+    //         />
+    //       </div>
+    //     </div>
+    //   ),
+    // },
+  ];
+
+  return (
+    <div className="min-h-screen overflow-x-hidden bg-[#1B232E]">
+      {/* <SplashCursor /> */}
+      <div className="absolute top-0 right-0 bottom-0 w-full h-full">
+        <Squares
+          speed={0.1}
+          squareSize={40}
+          direction="diagonal" // up, down, left, right, diagonal
+          borderColor="#454B29"
+          hoverFillColor="#1B232E"
+        />
+      </div>
+      <div className="container mx-auto h-screen">
+        <div className="grid grid-cols-12">
+          <div className="col-span-5 relative">
+            <Lanyard position={[0, 0, 12]} gravity={[0, -40, 0]} />
+            <div className="absolute top-20 right-10">
+              <CircularText
+                text="WEB*DATA*AI*ML*"
+                onHover="speedUp"
+                spinDuration={20}
+                // className="absolute top-20 right-20"
+              />
+            </div>
+          </div>
+
+          <div className="col-span-7 ml-4 lg:mr-24">
+            <div className="flex items-center h-full">
+              <div className="flex flex-col gap-6">
+                <AnimatedContent
+                  className="flex items-center gap-2"
+                  distance={150}
+                  direction="horizontal"
+                  reverse={false}
+                  duration={1.2}
+                  ease="bounce.out"
+                  initialOpacity={0.2}
+                  animateOpacity
+                  scale={1.1}
+                  threshold={0.2}
+                  delay={0.3}
+                >
+                  <div className="flex items-center gap-2">
+                    <h1 className="text-2xl text-white font-bold">
+                      I'm Ready For Job
+                    </h1>
+                    <RotatingText
+                      texts={[
+                        "Web Design",
+                        "Web Development",
+                        "Data Analyst",
+                        "AI Development",
+                      ]}
+                      mainClassName="px-2 sm:px-2 md:px-3 bg-[#C5F10F] text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg text-2xl font-bold inline-flex transition-all"
+                      staggerFrom={"last"}
+                      initial={{ y: "100%" }}
+                      animate={{ y: 0 }}
+                      exit={{ y: "-120%" }}
+                      staggerDuration={0.025}
+                      splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                      transition={{
+                        type: "spring",
+                        damping: 30,
+                        stiffness: 400,
+                      }}
+                      rotationInterval={2000}
+                    />
+                  </div>
+                </AnimatedContent>
+
+                <div className="flex flex-col items-start">
+                  <SplitText
+                    text="I'm Marchel Adias Pradana"
+                    className="text-2xl font-semibold text-center"
+                    delay={50}
+                    duration={0.6}
+                    ease="power3.out"
+                    splitType="chars"
+                    from={{ opacity: 0, y: 40 }}
+                    to={{ opacity: 1, y: 0 }}
+                    threshold={0.1}
+                    rootMargin="-100px"
+                    textAlign="center"
+                  />
+                  <SplitText
+                    text="FullStack Developer"
+                    className="text-2xl font-semibold text-center text-[#C5F10F]"
+                    delay={75}
+                    duration={0.6}
+                    ease="power3.out"
+                    splitType="chars"
+                    from={{ opacity: 0, y: 40 }}
+                    to={{ opacity: 1, y: 0 }}
+                    threshold={0.1}
+                    rootMargin="-100px"
+                    textAlign="center"
+                  />
+                </div>
+
+                <div className="">
+                  <BlurText
+                    text="I am a fresh graduated at Universitas Pembangunan Negeri 'VETERAN' Jawa Timur, starting to look for roles in the web development, data analyst, or AI Engineer. I really enjoy learning things related to technology. My dream right now is to become a Web Developer at a well-known company. If you also have the same interests as me. "
+                    delay={75}
+                    animateBy="words"
+                    direction="top"
+                    className="text-xl mb-8"
+                  />
+
+                  <div className="flex items-center">
+                    <GradientText
+                      colors={[
+                        "#40ffaa",
+                        "#C5F10F",
+                        "#40ffaa",
+                        "#C5F10F",
+                        "#40ffaa",
+                      ]}
+                      animationSpeed={3}
+                      showBorder={false}
+                      className="px-6 py-3 rounded-lg border"
+                    >
+                      <a href="https://drive.google.com/file/d/1rkRzwk9v5aBDQOTPfAh9XShVoECAAJKQ/view?usp=sharing">
+                        See my CV
+                      </a>
+                    </GradientText>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Section */}
+
+      {/* <div className="container mx-auto min-h-screen"> */}
+      <div className="">
+        <ScrollVelocity
+          texts={["Marchel Adias Pradana -", "Web - Data - AI -"]}
+          // className="custom-scroll-text"
+        />
+        <Timeline data={data}/>
+      </div>
     </div>
   );
 }
